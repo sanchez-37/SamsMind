3a362eb8e5fb73180d7e9fb096099218b13e8309
Samuel Sanchez <sanchezsamuel37@gmail.com>
2020-03-28 23:27:25 -0700

    added template for linear_algebra page

---
+++ linear_independence.js
@@ -0,0 +1,12 @@
+function linear_independence(){
+
+	var e = document.getElementById("vspace");
+	var vector_space = Number(e.options[e.selectedIndex].value);
+	var e = document.getElementById("numvectors");
+	var numberOfVectors = Number(e.options[e.selectedIndex].value);
+
+
+
+
+
+}
